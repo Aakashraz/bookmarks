4e9a3cbb914163883bab692547ba949840846fb5
Aakashraz <aakashraz@outlook.com>
2025-01-10 10:34:08 +0545

    new register and register_done htmls created

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    # This method is executed when the form is validated by calling its is_valid() method.
+    # You can provide a clean_<fieldname>() method to any
+    # of your form fields to clean the value or raise form validation errors for a specific field.
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
