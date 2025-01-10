ce0def758314662452857e9c8e8fe7315da074f3
Aakashraz <aakashraz@outlook.com>
2025-01-10 10:33:32 +0545

    register method added to register new users

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -39,3 +39,28 @@ def dashboard(request):
         'account/dashboard.html',
         {'section': 'dashboard'}
     )
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # For security reasons, instead of saving the raw
+            # password entered by the user, we use the set_password() method of the user model. This method
+            # handles password hashing before storing the password in the database.
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user}
+                          )
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form}
+                  )
