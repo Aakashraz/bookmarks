22387c81a83f81c9f680823cf8c7f194d60fd9f5
Aakashraz <aakashraz@outlook.com>
2025-01-23 14:11:48 +0545

    view method image_create() added to handle form submission

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        # data=request.POST - populates the form with the submitted data from POST request
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # assign current user to the item
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image was successfully added')
+            # redirect to new created item detail view
+            return redirect(new_image.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+    return render(
+        request,
+        'image/image/create.html',
+        {'section': 'images', 'form': form}
+    )
