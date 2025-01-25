1461d1dc16ad469c3c1659fb4970a5d52296fedb
Aakashraz <aakashraz@outlook.com>
2025-01-25 23:37:16 +0545

    added bookmarklet_launcher.js file and working on integrity error

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -6,17 +6,21 @@ from .forms import ImageCreateForm
 
 @login_required
 def image_create(request):
+    print(f"User: {request.user}, Authenticated: {request.user.is_authenticated}")
+
     if request.method == 'POST':
         # form is sent
         form = ImageCreateForm(data=request.POST)
         # data=request.POST - populates the form with the submitted data from POST request
         if form.is_valid():
-            # form data is valid
+            # form data is validx
             cd = form.cleaned_data
             new_image = form.save(commit=False)
-            # assign current user to the item. This is how we will know who uploaded each image.
-            new_image.user = request.user
-            new_image.save()
+            print(f"Before saving: User = {request.user}, Authenticated = {request.user.is_authenticated}")
+            if request.user.is_authenticated:
+                # assign current user to the item. This is how we will know who uploaded each image.
+                new_image.user = request.user
+                new_image.save()
             messages.success(request, 'Image was successfully added')
             # redirect to new created item detail view
             return redirect(new_image.get_absolute_url())
