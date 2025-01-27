8f8fb698f9d264a872983191b1f8858259ee76d5
Aakashraz <aakashraz@outlook.com>
2025-01-27 14:02:06 +0545

    minor changes, still working on bookmarklet launcher

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -1,19 +1,20 @@
-const siteUrl = '//127.0.0.1:8000/';
-const styleUrl = siteUrl + 'static/css/bookmarklet.css';
-const minWidth = 250;
-const minHeight = 250;
+javascript: (function () {
+    const siteUrl = 'https://127.0.0.1:8000/';
+    const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+    const minWidth = 250;
+    const minHeight = 250;
 
 
 // load CSS
-var head = document.getElementsByTagName('head')[0];
+    var head = document.getElementsByTagName('head')[0];
 // It first gets a reference to the <head> element of the current web page
-var link = document.createElement('link');
+    var link = document.createElement('link');
 // It then creates a new <link> element
-link.rel = 'stylesheet';
-link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
 // The rel and type attributes of the <link> element are set.
-link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
-head.appendChild(link);
+    link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+    head.appendChild(link);
 
 // The above code generates an object equivalent to the following JavaScript code and appends it to
 // the <head> element of the HTML page:
@@ -21,62 +22,63 @@ head.appendChild(link);
 
 
 // load HTML
-var body = document.getElementsByTagName('body')[0]
+    var body = document.getElementsByTagName('body')[0]
 // Define an HTML template as a string that will be injected into the page
-boxHtml = `
+    boxHtml = `
     <div id="bookmarklet">
 <!--    A close button (<a> with &times; which displays an "×" symbol)-->
     <a href="#" id="close">&times;</a>
     <h1>Select an image to bookmark:</h1>
     <div class="images"></div>
     <!-- This container is initially empty and will be filled with the images found on the site. -->
-</div>`;
-body.innerHTML += boxHtml;
+    </div>`;
+    body.innerHTML += boxHtml;
 // body.innerHTML += boxHtml appends the entire HTML template to the existing content of the <body> element.
 // The += operator means this new content is added to the end of whatever already exists in the body,
 // without replacing the existing content.
 
 // function to launch the bookmarklet
-function bookmarkletLaunch() {
-    let bookmarklet = document.getElementById('bookmarklet');
-    var imagesFound = bookmarklet.querySelector('.images');
+    // define a global function
+    window.bookmarkletLaunch = function() {
+        let bookmarklet = document.getElementById('bookmarklet');
+        var imagesFound = bookmarklet.querySelector('.images');
 
-    // the image container is cleared by setting its innerHTML attribute to an empty string
-    imagesFound.innerHTML = '';
-    // display bookmarklet by setting the 'display CSS' property to 'block'
-    bookmarklet.style.display = 'block';
+        // the image container is cleared by setting its innerHTML attribute to an empty string
+        imagesFound.innerHTML = '';
+        // display bookmarklet by setting the 'display CSS' property to 'block'
+        bookmarklet.style.display = 'block';
 
-    // close event
-    // When  users click the element, the bookmarklet's main container is hidden by setting
-    // its 'display' property to 'none'.
-    bookmarklet.querySelector('#close')
-        .addEventListener('click', function() {
-            bookmarklet.style.display = 'none'
+        // close button event
+        // When  users click the element, the bookmarklet's main container is hidden by setting
+        // its 'display' property to 'none'.
+        bookmarklet.querySelector('#close')
+            .addEventListener('click', function() {
+                bookmarklet.style.display = 'none'
+            })
+
+        // find images in the DOM with the minimum dimensions
+        let images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+        // The above line will find the DOM elements whose src attribute finishes with .jpg, .jpeg and .png respectively
+        console.log(`TOTAL IMAGES FOUND: ${images.length}`);
+
+        // The arrow function syntax image => {} is essentially syntactic sugar for
+        // a traditional function declaration function(image) {}.
+        images.forEach(image => {
+            if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
+                var imageFound = document.createElement('img');
+                imageFound.src = image.src;
+                imagesFound.append(imageFound);
+                console.log("IMAGES ADDED TO BOOKMARKLET");
+            }
+            // A new <img> element is created for each image found, where the src source URL attribute is
+            // copied from the original image and added to the imagesFound container
         })
 
-    // find images in the DOM with the minimum dimensions
-    let images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
-    // The above line will find the DOM elements whose src attribute finishes with .jpg, .jpeg and .png respectively
-    console.log(`TOTAL IMAGES FOUND: ${images.length}`);
-
-    // The arrow function syntax image => {} is essentially syntactic sugar for
-    // a traditional function declaration function(image) {}.
-    images.forEach(image => {
-        if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
-            var imageFound = document.createElement('img');
-            imageFound.src = image.src;
-            imagesFound.append(imageFound);
-            console.log("IMAGES ADDED TO BOOKMARKLET");
+        // Add a check if no images were found
+        if (imagesFound.children.length === 0) {
+            console.warn('NO IMAGES MEETING SIZE CRITERIA FOUND.')
         }
-        // A new <img> element is created for each image found, where the src source URL attribute is
-        // copied from the original image and added to the imagesFound container
-    })
-
-    // Add a check if no images were found
-    if (imagesFound.children.length === 0) {
-        console.warn('NO IMAGES MEETING SIZE CRITERIA FOUND.')
     }
-}
-
 // launch the bookmarklet
-bookmarkletLaunch();
+    window.bookmarkletLaunch();
+})();
--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,17 +1,21 @@
-(function(){
+javascript: (function(){
     if(!window.bookmarklet) {
-        // t creates a new <script> element, sets its src attribute to the URL of the bookmarklet.js file, and
+        // it creates a new <script> element, sets its src attribute to the URL of the bookmarklet.js file, and
         // appends it to the <body>.
-        bookmarklet_js = document.body.appendChild(document.createElement('script'));
+        const bookmarklet_js = document.createElement('script');
 
         bookmarklet_js.src = '//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);
         // Using a random number, we prevent the browser from loading
         // the file from the browser’s cache. If the bookmarklet JavaScript has been previously loaded,
         // the different parameter value will force the browser to load the script from the source URL
         // again. This way, we make sure the bookmarklet always runs the most up-to-date JavaScript code.
-        window.bookmarklet = true;
-    }
-    else {
+
+        // wait for the script to load before marking bookmarklet as loaded
+        bookmarklet_js.onload = () => {
+            window.bookmarklet = true;
+        }
+        document.body.appendChild(bookmarklet_js);
+    } else {
         bookmarkletLaunch();
         // If window.bookmarklet is defined and has a truthy value, the bookmarkletLaunch() function
         // is executed. We will define bookmarkletLaunch() as a global function in the bookmarklet.js
