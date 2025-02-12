64f5185d4bbf2eed5cf9ea01620669baaa7b4a59
Aakashraz <aakashraz@outlook.com>
2025-02-12 17:24:19 +0545

    a new intermediate model named Contact created

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -13,3 +13,24 @@ class Profile(models.Model):
     def __str__(self):
         return f'Profile of {self.user.username}'
 
+
+# Intermediate model to build relationship between users.
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_ser',
+        on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_to_set',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['-created'])],
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
