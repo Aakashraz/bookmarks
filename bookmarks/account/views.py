9624e495c4e65eee974b7897225943a784cd004c
Aakashraz <aakashraz@outlook.com>
2025-03-04 17:42:57 +0545

    user_follow() method added

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 
 def user_login(request):
@@ -128,3 +129,21 @@ def user_detail(request, username):
         'account/user/detail.html',
         {'section': 'people', 'user': user}
     )
+
+# Adding user follow/unfollow actions with JavaScript
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
