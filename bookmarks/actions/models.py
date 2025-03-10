a3cb93c3f4ebe593c5a94ece6d767e64488bdded
Aakashraz <aakashraz@outlook.com>
2025-03-10 23:45:22 +0545

    Action model added to the administration site and working with contenttypes framework

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Action(models.Model):
@@ -8,10 +10,24 @@ class Action(models.Model):
                              )
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True, null=True,
+        related_name='target_obj',
+        on_delete=models.CASCADE
+    )
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+
+    # target_ct: A ForeignKey field that points to the ContentType model
+    # target_id: A PositiveIntegerField for storing the primary key of the related object
+    # target: A GenericForeignKey field to the related object based on the combination of the two
+    # previous fields
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
+            models.Index(fields=['target_ct', 'target_id']),
         ]
         ordering = ['-created']
 
