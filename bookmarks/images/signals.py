b69d40e181db4736f1d6984eead0f78414c53910
Aakashraz <aakashraz@outlook.com>
2025-03-29 14:39:29 +0545

    signal handlers registered in images.apps.py inside the ready() method

--- bookmarks/images/signals.py
+++ bookmarks/images/signals.py
@@ -7,3 +7,15 @@ from .models import Image
 def users_like_changed(sender, instance, **kwargs):
     instance.total_likes = instance.users_like.count()
     instance.save()
+
+# @receiver:
+# A decorator provided by Django to connect a function (the signal handler) to a signal.
+# It lets Django know which function to call when the signal is emitted.
+
+# m2m_changed:
+# Specifies that this function should be called whenever there is a change in a Many-to-Many field.
+
+# Image.users_like.through references that intermediate model.
+
+# instance:
+# The instance of the Image model whose users_like field has changed. This is the image for which likes have been added or removed.
